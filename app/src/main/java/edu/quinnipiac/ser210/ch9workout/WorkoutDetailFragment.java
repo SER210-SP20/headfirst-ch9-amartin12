da2bd0ab05304ce8bc2772ef3bf46955e7482075
Alexandra Martin <amartin1@quinnipiac.edu>
2020-02-25 16:10:26 -0500

    Ch 9 Workout

---
+++ app/src/main/java/edu/quinnipiac/ser210/ch9workout/WorkoutDetailFragment.java
@@ -0,0 +1,56 @@
+package edu.quinnipiac.ser210.ch9workout;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WorkoutDetailFragment extends Fragment {
+
+    private long workoutId;
+
+    public WorkoutDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if (view != null) {
+            TextView title = (TextView) view.findViewById(R.id.textTitle);
+            Workout workout = Workout.workouts[(int) workoutId];
+            title.setText(workout.getName());
+            TextView description = (TextView) view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescription());
+
+        }
+    }
+
+
+    //Will set the value of the workout id
+    public void setWorkout(long id) {
+        this.workoutId = id; }
+
+}
